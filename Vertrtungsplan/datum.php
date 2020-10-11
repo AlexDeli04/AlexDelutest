1fb98a9eed356d59edfa82e8d2c027c05d8d3275
AlexDeli04 <72705361+AlexDeli04@users.noreply.github.com>
2020-10-11 19:41:27 +0200

    Add files via upload

---
+++ Vertrtungsplan/datum.php
@@ -0,0 +1,13 @@
+<?
+$variable = "Dies ist ein Test.";
+?>
+
+<html>
+	<head>
+		<title>Teseite</title>
+	</head>
+
+	<body>
+		Hier Bitte Wochentag und Datum eingeben in [wochentag, den xx.xx.xxxx] <p> wochentag, den xx.xx.xxxx <? echo $varbiale ?></p>
+	</body>  
+</html>
